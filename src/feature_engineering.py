4492e79e0ca5bdae3be1badaa43204affc856ddf
Nicky Ho <nicky35793579@gmail.com>
2025-04-07 23:41:06 -0700

    added teammate and age improvement features (teammate usage and younger players improving more

--- src/feature_engineering.py
+++ src/feature_engineering.py
@@ -20,6 +20,43 @@ def prepare_training_data(df, target_columns):
         suffixes=('', '_next')
     )
 
+    team_features = []
+
+    for idx, row in merged.iterrows(): 
+        team = row["TEAM_ID"]
+        season = row["SEASON"]
+        player_id = row["PLAYER_ID"]
+
+        teammates = merged [
+            (merged["TEAM_ID"] == team) &
+            (merged["SEASON"] == season) &
+            (merged["PLAYER_ID"] != player_id)
+        ]
+
+        usage_sum = teammates["USG_PCT"].sum() if "USG_PCT" in merged.columns else 0 
+        avg_usage = teammates["USG_PCT"].mean() if "USG_PCT" in merged.columns else 0
+        avg_ast = teammates["AST"].mean()
+        avg_reb = teammates["REB"].mean()
+
+        team_features.append({
+            "TEAM_USAGE_TOTAL": usage_sum,
+            "TEAMMATE_AVG_USAGE": avg_usage,
+            "TEAMMATE_AVG_AST": avg_ast,
+            "TEAMMATE_AVG_REB": avg_reb
+        })
+
+    teammate_context = pd.DataFrame(team_features)
+    merged = pd.concat([merged.reset_index(drop=True), teammate_context], axis=1)
+
+    #add time aware features
+    merged["SEASON_NUM"] = merged["SEASON"].apply(lambda x: int(x.split("-")[0]))
+
+    #compute league-wide season averages for all target stats
+    league_avg = merged.groupby("SEASON_NUM")[[f"NEXT_{col}" for col in target_columns]].mean().reset_index()
+    league_avg.columns = ["SEASON_NUM"] + [f"LEAGUE_AVG_NEXT_{col}" for col in target_columns]
+
+    merged = pd.merge(merged, league_avg, on="SEASON_NUM", how="left")
+
     return merged
 
 
